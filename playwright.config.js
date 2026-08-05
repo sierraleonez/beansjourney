c11d9dcb5dacb3b9d82382a0193981e56bb65603
Almer Andhika <almer@MIFX-IT-ALMER.local>
2026-08-05 18:08:49 +0700

    build(e2e): fix e2e configuration

--- playwright.config.js
+++ playwright.config.js
@@ -5,11 +5,14 @@ const baseURL = `http://127.0.0.1:${PORT}`;
 
 export default defineConfig({
     testDir: './e2e',
-    timeout: 30_000,
+    timeout: 180_000,
+    stdout: 'pipe',
+    stderr: 'pipe',
     fullyParallel: false,
     workers: 1,
     retries: 0,
     reporter: 'list',
+
     use: {
         baseURL,
         trace: 'retain-on-failure',
